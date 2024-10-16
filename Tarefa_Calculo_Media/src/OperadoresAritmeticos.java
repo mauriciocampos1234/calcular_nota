2efac4c69af8853656593113fdc3f9489409e54a
mauriciocampos1234 <156216039+mauriciocampos1234@users.noreply.github.com>
2024-10-16 15:02:37 -0300

    mod 8

---
+++ Tarefa_Calculo_Media/src/OperadoresAritmeticos.java
@@ -0,0 +1,21 @@
+/**
+ * @author MauricioCampos
+ */
+
+public class OperadoresAritmeticos {
+    public static void main(String[] args) {
+        int nota1 = 10;
+        int nota2 = 6;
+        int nota3 = 8;
+        int nota4 = 4;
+
+        int nota5 = (nota1 + nota2 + nota3 + nota4) / 4;
+
+        System.out.println(nota5);
+    }
+}
+
+
+
+
+
